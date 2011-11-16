8e4f9a6497d52c1d0731c3b5812daba11794c1ca
vlad12354 <vlad12354@gmail.com>
2011-11-16 21:43:00 +0000

    + Funciona la comparación a través de arbol + Añadida clase para hacer pruebas al MyTreeSimilarityFunction.

--- src/es/ucm/fdi/isbc/funcSimilitud/MyTreeSimilarityFunction.java
+++ src/es/ucm/fdi/isbc/funcSimilitud/MyTreeSimilarityFunction.java
@@ -1,11 +1,10 @@
 package es.ucm.fdi.isbc.funcSimilitud;
 
-import java.util.Enumeration;
-
 import javax.swing.JTree;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
 
+import jcolibri.cbrcore.CBRCase;
 import jcolibri.exception.NoApplicableSimilarityFunctionException;
 import jcolibri.method.retrieve.NNretrieval.similarity.LocalSimilarityFunction;
 
@@ -16,38 +15,40 @@ public class MyTreeSimilarityFunction implements LocalSimilarityFunction {
 		this.tree = tree;
 	}
 
-	public TreePath findByName(JTree tree, String name) {
+	public String findByName(JTree tree, String name) {
 		TreeNode root = (TreeNode) tree.getModel().getRoot();
-		return find(tree, new TreePath(root), name, 0, true);
+		String pathRoot = root.toString();
+		return find(root, pathRoot, name, 0, true);
 	}
 
-	private TreePath find(JTree tree, TreePath parent, Object node2Comp, int depth, boolean byName) {
-		TreeNode node = (TreeNode) parent.getLastPathComponent();
-		Object o = node;
+	private String find(TreeNode parentNode, String parentPath, Object node2Comp, int depth, boolean byName) {
+		Object o = parentNode;
 
-		// If by name, convert node to a string
+		// Si es by name, entonces comparamos los strings del nodo
 		if (byName) {
 			o = o.toString();
 		}
 
-		// If equal, go down the branch
+		// Si no coinciden, habrá que buscar en los hijos de este nodo.
 		if (!o.equals(node2Comp)) {
 
-			// Traverse children
-			if (node.getChildCount() >= 0) {
-				for (Enumeration e = node.children(); e.hasMoreElements();) {
-					TreeNode n = (TreeNode) e.nextElement();
+			// Buscamos en todos los hijos. Recursión en profundidad
+			if (!parentNode.isLeaf() && parentNode.getChildCount() >= 0) {
+				for (int n = 0; n < parentNode.getChildCount(); n++) {
+					TreeNode childNode =  parentNode.getChildAt(n);
 					// Añadimos el hijo a la path, para buscar luego con el.
-					TreePath path = parent.pathByAddingChild(n); 
-					TreePath result = find(tree, path, node2Comp, depth + 1, byName);
-					// Found a match
-					if (result != null) {
-						return result;
-					}
+					String childPath = parentPath+"/"+childNode.toString(); 
+					String result = find(childNode, childPath, node2Comp, depth + 1, byName);
+	                // Hemos encontrado ya el nodo?
+	                if (result != null) {
+	                    return result;
+	                }
 				}
 			}
-		} else {
-			return parent;
+			
+		} 
+		else { // Coinciden, luego devuelvo la path construida.
+			return parentPath;
 		}
 
 		// No match at this branch
@@ -63,14 +64,37 @@ public class MyTreeSimilarityFunction implements LocalSimilarityFunction {
 	@Override
 	public double compute(Object caseObject, Object queryObject) throws NoApplicableSimilarityFunctionException {
 		// Obtenemos el path de la query
-		TreePath path = this.findByName(tree, (String)queryObject);
-		System.out.println(path.toString());
+		String pathQuery = (String) queryObject;
+		System.out.println(pathQuery);
+		String[] sQuery, sCase;
+		sQuery = pathQuery.split("/");
+		pathQuery = this.findByName(tree, sQuery[sQuery.length-1]); //Buscamos solo con el último dato.
+		System.out.println(pathQuery);
+		//Volvemos a separarlo pero ahora con la path entera
+		sQuery = pathQuery.split("/");
+		
 		// Obtenemos el path del caso a comparar
+		String pathCase = (String) caseObject;
+		sCase = pathCase.split("/");
 		
 		// Comparamos los paths para obtener la profundidad del nodo común
+		//Sabemos que sCase[0] es un valor corrupto y el de sQuery es madrid. Asi que comparamos con sCase[1] == sQuery[1] pero este no hace falta.
+		double profCoQ = 0.0; //Profundidad (Case v Query)
+		boolean coincidencia = false;
+		int i = Math.min(sCase.length, sQuery.length) - 1;
+		while ( i > 0 && !coincidencia){
+			coincidencia = sCase[i].equalsIgnoreCase(sQuery[i]);
+			i--;
+		}
+		if(coincidencia)
+			profCoQ = i + 1;
+		else
+			profCoQ = 0;
 		
 		// Calculamos similitud a devolver
-		return 0;
+		double maxprof = Math.max(sCase.length, sQuery.length);
+		double result = profCoQ/maxprof;
+		return result;
 	}
 
 }
