afafc8763b3ccb771143f65f2d571d635304ebca
gepozuelo <gepozuelo@68ca04bf-b1c6-05cf-da42-ab8a2c9039c3>
2011-11-19 17:58:07 +0000

    + Ahora cuando el usuario no mete información en un campo, no se utiliza en la función de similitud. + Añadido un código en PanelCaract.java, para la obtención de las coordenadas a partir de la localización (que todavía no funciona)

--- trunk/src/es/ucm/fdi/isbc/gui/PanelCaract.java
+++ trunk/src/es/ucm/fdi/isbc/gui/PanelCaract.java
@@ -2,6 +2,12 @@ package es.ucm.fdi.isbc.gui;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Vector;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
@@ -9,6 +15,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
+import es.ucm.fdi.isbc.viviendas.representacion.Coordenada;
 import es.ucm.fdi.isbc.viviendas.representacion.DescripcionVivienda;
 import es.ucm.fdi.isbc.viviendas.representacion.DescripcionVivienda.EstadoVivienda;
 import es.ucm.fdi.isbc.viviendas.representacion.DescripcionVivienda.TipoVivienda;
@@ -17,8 +24,8 @@ import es.ucm.fdi.isbc.viviendas.representacion.DescripcionVivienda.TipoVivienda
 public class PanelCaract extends JPanel{
 	private JTextArea[] textAreas;
 	private JLabel[] labels;
-	@SuppressWarnings("rawtypes")
 	private JComboBox tipoVivienda, estadoVivienda;
+	private Coordenada coordenada;
 	
 	public PanelCaract(){
 		super();
@@ -42,7 +49,7 @@ public class PanelCaract extends JPanel{
 		for (JLabel l: labels){
 			add(l);
 			textAreas[i] = new JTextArea(1,20);
-			textAreas[i].setText("1");
+			textAreas[i].setText("");
 			textAreas[i].setBorder(BorderFactory.createLineBorder(Color.black));
 			add(textAreas[i]);
 			i++;
@@ -56,21 +63,100 @@ public class PanelCaract extends JPanel{
 		caract.setEstado((EstadoVivienda)estadoVivienda.getSelectedItem());
 		// Si la conversión es errónea suponemos que el usuario ha introducido datos
 		try{
-			caract.setSuperficie(Integer.parseInt(textAreas[0].getText()));
+			if (textAreas[0].getText() != "")
+				caract.setSuperficie(Integer.parseInt(textAreas[0].getText()));
+			else caract.setSuperficie(null);
 		}catch(NumberFormatException e){}
 		try{
-			caract.setHabitaciones(Integer.parseInt(textAreas[1].getText()));
+			if (textAreas[1].getText() != "")
+				caract.setHabitaciones(Integer.parseInt(textAreas[1].getText()));
+			else caract.setHabitaciones(null);
 		}catch(NumberFormatException e){}
 		try{
-			caract.setBanios(Integer.parseInt(textAreas[2].getText()));
+			if (textAreas[2].getText() != "")
+				caract.setBanios(Integer.parseInt(textAreas[2].getText()));
+			else caract.setBanios(null);
 		}catch(NumberFormatException e){}
 		try{
-			caract.setPrecioMedio(Integer.parseInt(textAreas[3].getText()));
+			if (textAreas[3].getText() != "")
+				caract.setPrecioMedio(Integer.parseInt(textAreas[3].getText()));
+			else caract.setPrecioMedio(null);
 		}catch(NumberFormatException e){}
 		try{
-			caract.setPrecioZona(Integer.parseInt(textAreas[4].getText()));
+			if (textAreas[4].getText() != "")
+				caract.setPrecioZona(Integer.parseInt(textAreas[4].getText()));
+			else caract.setPrecioZona(null);
 		}catch(NumberFormatException e){}
-		caract.setLocalizacion(textAreas[5].getText());
+		if (textAreas[5].getText() != ""){
+			caract.setLocalizacion(textAreas[5].getText());
+			// A partir de la localización obtenemos las coordenadas
+			coordenada = new Coordenada();
+			try {
+				this.getStringCoordinate(textAreas[5].getText());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			caract.setCoordenada(coordenada);
+		}
 		return caract;
 	}
+	
+	   private void getStringCoordinate(String Address) throws IOException {
+	        URL url = new URL(getGeoCoordinateURL(Address));
+	        InputStream stream = url.openStream();
+	        BufferedReader buffer = new BufferedReader(new InputStreamReader(stream));
+	        String web = buffer.readLine();
+
+	        String coordenadas[] = Split(web, ',');
+	        coordenada.setLatitud(Double.parseDouble(coordenadas[2]));
+	        coordenada.setLongitud(Double.parseDouble(coordenadas[3]));
+	    }
+	   
+
+	    private String[] Split(String Text, int Character) {
+	        Vector vector = new Vector();
+
+	        int current = 0;
+	        int prior = 0;
+
+	        while ((current = Text.indexOf(Character, prior)) >= 0) {
+	            vector.addElement(Text.substring(prior, current));
+	            prior = current + 1;
+	        }
+
+	        vector.addElement(Text.substring(prior));
+	        String[] split = new String[vector.size()];
+	        vector.copyInto(split);
+
+	        return split;
+	    } 
+
+	    private String getGeoCoordinateURL(String Address) {
+	        return "http://maps.google.com/maps/geo?q=" + UrlEncode(Address) +
+	                "&output=csv&key=" + getKey();
+	    }
+	    
+	    private String getKey() {
+	    	// TODO: De momento pongo esto 
+			return "1";
+		}
+
+		private String UrlEncode(String Url) {
+	        StringBuffer buffer = new StringBuffer();
+	        char word;
+	        for (int i = 0; i < Url.length(); i++){
+	            word = Url.charAt(i);
+	            if ((word >= '0' && word <= '9') || (word >= 'A' && word <= 'Z') ||
+	                    (word >= 'a' && word <= 'z')) {
+	                buffer.append(word);
+	            }
+	            else {
+	                buffer.append("%").append(Integer.toHexString((int) Url.charAt(i)));
+	            }
+	        }
+	        return buffer.toString();
+	    }
+
+
 }
