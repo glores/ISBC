c16c32dd4ec2805a4686af6000950c59e5a7cde5
m.gallardo.casu@gmail.com <m.gallardo.casu@gmail.com>
2011-12-28 07:10:21 +0000

    Cambios de interfaz (layouts, ventanaResultados (sin acabar))

--- PR3/src/es/ucm/fdi/isbc/gui/PanelDiversidad.java
+++ PR3/src/es/ucm/fdi/isbc/gui/PanelDiversidad.java
@@ -1,6 +1,9 @@
 package es.ucm.fdi.isbc.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Scrollbar;
+import java.awt.Toolkit;
 
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
@@ -10,17 +13,32 @@ import javax.swing.JPanel;
  *
  */
 
-public class PanelDiversidad extends JPanel{
+public class PanelDiversidad extends JPanel
+{
+	
 	private static final long serialVersionUID = 1L;
 	
-	public PanelDiversidad(){
-		BoxLayout box = new BoxLayout(this, BoxLayout.X_AXIS);
-		setLayout(box);
-		setMinimumSize(new Dimension(100, 200));
+	public PanelDiversidad()
+	{
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		Scrollbar scroll = new Scrollbar(Scrollbar.VERTICAL);
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.add(scroll, BorderLayout.EAST);
+		
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		dim.setSize(dim.width - 100, dim.height - 100);
+		
+		setMinimumSize(new Dimension((int) (dim.width * 0.73), (int) (dim.height * 0.78)));
+		setSize(new Dimension((int) (dim.width * 0.73), (int) (dim.height * 0.78)));
+
+		this.add(panel);
 	}
 	
-	// TODO una vez obtenidas las viviendas con diversidad de 3 a 5, meterlas en el panel
-	public void setViviendasDiversas(){
+	// Una vez obtenidas las viviendas con diversidad de 3 a 5, meterlas en el panel
+	
+	public void setViviendasDiversas()
+	{
 		
 	}
 
