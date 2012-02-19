876dba88cc89f38789757847165011c38ab9b653
gepozuelo <gepozuelo@gmail.com>
2012-02-19 22:34:53 +0000

    - Eliminado lo último que comiteó Marco, ya que esos cambios no hacían nada nuevo sobre lo que había hecho Vlad. + Cambiado NUMSELECTCASOS a 3 para que aumente un poquitín la confianza.

--- PR4/src/es/ucm/fdi/isbc/gui/PanelCaract.java
+++ PR4/src/es/ucm/fdi/isbc/gui/PanelCaract.java
@@ -11,7 +11,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Vector;
-import java.util.regex.Pattern;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
