ac2312a10477f4d57288046b39a68898b53cf263
gepozuelo <gepozuelo@gmail.com>
2012-03-30 15:34:52 +0000

    + Adaptada la interfaz para el apartado de marcado de imágenes + Añadida la galería que usamos en la PR3, para gestionar las imágenes
    Notas:
    - Al realizar las consultas, se recuperan todas las path de las imágenes, pero de momento sólo se muestra la primera, porque prefiero que el tema de la galería lo haga alguno de vosotros que la manejaréis mejor que yo ;)
    - La interfaz está copiada de la captura que puso la profesora en el enunciado de la práctica, por lo que supongo que en la pestaña de marcar, se deberá poder elegir de alguna manera una imagen y con los comboBox seleccionar relación e individuo, dependiendo de si se quiere relacionar un individuo con la imagen o simplemente categorizar la imagen, en cuyo caso sólo se seleccionaría relación
    - Faltaría cargar las relaciones que tenemos en nuestra ontología en estos comboBox y si selecciona algún individuo en el otro comboBox filtrar las relaciones que están disponibles para ese individuo
    - En el test9 de ontobridge muestra cómo crear y eliminar relaciones

--- PR5/src/gui/Main.java
+++ PR5/src/gui/Main.java
@@ -1,12 +1,7 @@
 package gui;
 
-import java.text.DecimalFormat;
-import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -30,7 +25,6 @@ public class Main {
 		Logger log = Logger.getLogger("");
 		for (Handler h : log.getHandlers())
 			log.removeHandler(h);
-		log = crearLog("CP");
 		
 		try {
 			UIManager.setLookAndFeel(MAIN_LANDF);
@@ -45,34 +39,6 @@ public class Main {
 			log.severe(e.getMessage());
 		}
 		
-	}
-	
-	private static Logger crearLog(String nombre) {
-		Logger log;
-		log = Logger.getLogger(nombre);
-		log.addHandler(new ConsoleHandler());
-		log.getHandlers()[0].setFormatter(new SimpleFormatter() {
-			DecimalFormat fmt = new DecimalFormat("000");
-			long previousMillis = System.currentTimeMillis();
-			int stanza = 0;
-
-			@Override
-			public synchronized String format(LogRecord record) {
-				String s = (record.getMillis() - previousMillis > 500) ? "\n *** "
-						+ (++stanza) + " *** \n\n"
-						: "";
-				previousMillis = record.getMillis();
-				return s + fmt.format(previousMillis % 1000) + " "
-						+ record.getLevel() + " -- " + record.getMessage()
-						+ "\n";
-			}
-		});
-		/* configuracion del nivel de salida 'por defecto' de los logs
-		   opciones: FINEST, FINER, FINE, INFO, CONFIG, WARNING, SEVERE
-		   (es posible configurar los logs individuales a niveles distintos)*/
-		log.getHandlers()[0].setLevel(Level.ALL);
-		log.setLevel(Level.ALL);
-		return log;
 	}
 	
 }
