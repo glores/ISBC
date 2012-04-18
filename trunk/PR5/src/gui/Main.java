2f71a767c31191be88e7e7c285e0af8a31fa7f40
vlad12354 <vlad12354@68ca04bf-b1c6-05cf-da42-ab8a2c9039c3>
2012-04-18 02:42:33 +0000

    + Arreglado errores de la parte de Búsqueda, ahora está mucho más cerrado. + Interfaz mejorada (en búsqueda), botones mejorados, .... + Arreglado serios errores de marcado (cuando metías una nueva imagen, eso no hacía nada). Ahora se pueden añadir imagenes nuevas pero han de ser .jpg y las tienes que copiar a la carpeta img. + Se han quitado las relaciones residuales y los "path" de las relaciones (usando ontobridge, nada de guarreos). + Testeado el marcado y búsqueda a la vez. Lo que haces en uno repercute en el otro. - El boton de "save" de la ontologia no funciona, pero no es nuestra culpa. - Ontología ha vuelto a la anterior versión, para ser compatible.

--- trunk/PR5/src/gui/Main.java
+++ trunk/PR5/src/gui/Main.java
@@ -1,10 +1,10 @@
 package gui;
 
-//import java.util.logging.Handler;
-//import java.util.logging.Logger;
-//
-//import javax.swing.UIManager;
-//import javax.swing.UnsupportedLookAndFeelException;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 
 /**
@@ -15,31 +15,32 @@ package gui;
  * @author Marco Gallardo Casu
  *
  */
+
 public class Main {
-//	private static final String  MAIN_LANDF="de.javasoft.plaf.synthetica.SyntheticaOrangeMetallicLookAndFeel";
+	private static final String  MAIN_LANDF="de.javasoft.plaf.synthetica.SyntheticaOrangeMetallicLookAndFeel";
 //	private static final String  MAIN_LANDF="de.javasoft.plaf.synthetica.SyntheticaStandardLookAndFeel";
 //	private static final String  MAIN_LANDF="de.javasoft.plaf.synthetica.SyntheticaBlackEyeLookAndFeel";
 	
 	public static void main(String[] args) {
 		// configuracion de formato de salida de los logs
-//		Logger log = Logger.getLogger("CP");
-//		for (Handler h : log.getHandlers())
-//			log.removeHandler(h);
-//		
-//		try {
-////			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//			UIManager.setLookAndFeel(MAIN_LANDF);
+		Logger log = Logger.getLogger("CP");
+		for (Handler h : log.getHandlers())
+			log.removeHandler(h);
+		
+		try {
+//			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			UIManager.setLookAndFeel(MAIN_LANDF);
 			new Interfaz();
-//		} catch (ClassNotFoundException e) {
-//			log.severe(e.getMessage());
-//		} catch (InstantiationException e) {
-//			log.severe(e.getMessage());
-//		} catch (IllegalAccessException e) {
-//			log.severe(e.getMessage());
-//		} catch (UnsupportedLookAndFeelException e) {
-//			log.severe(e.getMessage());
-//		}
-//		
+		} catch (ClassNotFoundException e) {
+			log.severe(e.getMessage());
+		} catch (InstantiationException e) {
+			log.severe(e.getMessage());
+		} catch (IllegalAccessException e) {
+			log.severe(e.getMessage());
+		} catch (UnsupportedLookAndFeelException e) {
+			log.severe(e.getMessage());
+		}
+		
 	}
 	
 }
